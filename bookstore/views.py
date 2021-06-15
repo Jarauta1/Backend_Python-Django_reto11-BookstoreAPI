989b43b9754d1f2bcd3d10e584ed1cd59794aaad
Jarauta1 <diegojarauta8@gmail.com>
2021-06-15 19:19:40 +0200

    reto 11 finalizado

---
+++ bookstore/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+
+from .serializers import AuthorSerializer, BookSerializer, UserSerializer
+from .models import Author, Book
+from django.contrib.auth.models import User
+
+# Create your views here.
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all().order_by('author_id')
+    serializer_class = AuthorSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all().order_by('book_id')
+    serializer_class = BookSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = ['username']
